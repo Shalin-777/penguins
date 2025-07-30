0641b7b83c1f7f24d9c15c1801d30f4ae4635ed1
Shalin-777 <chaudharyshalin786@gmail.com>
2025-07-30 09:36:23 -0600

    s

--- app.py
+++ app.py
@@ -46,23 +46,25 @@ with st.expander("Data Preperation"):
   pass
 
 with st.sidebar:
-  st.header("Input Variables")
-  island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
-  bill_length_mm = st.slider("Bill length (mm)",32.1,59.6,43.9)
-  bill_depth_mm = st.slider("Bill depth (mm)",13.1,21.5,17.2)
-  flipper_length_mm = st.slider("Flipper length (mm)",172.0,231.0,201.0)
-  body_mass_g = st.slider("Body mass (g)",2700.0,6300.0,4207.0)
-  gender = st.selectbox("Gender",("male","female","That's it"))
+    st.header("Input Variables")
+    island = st.selectbox("Island", ("Biscoe","Dream", "Torgersen"))
+    bill_length_mm = st.slider("Bill length (mm)", 32.1, 59.6, 43.9)
+    bill_depth_mm = st.slider("Bill depth (mm)", 13.1, 21.5, 17.2)
+    flipper_depth_mm = st.slider("Flipper depth (mm)", 172.0, 231.0, 201.0)
+    body_mass_g = st.slider("Body mass (g)", 2700.0, 6300.0, 4207.0)
+    gender = st.selectbox("Gender", ("male", "female"))
 
-  data = {"island": island,
+    data = {
+        "island": island,
         "bill_length_mm": bill_length_mm,
         "bill_length_mm": bill_length_mm,
         "flipper_depth_mm": flipper_depth_mm,
         "body_mass_g": body_mass_g,
-        "gender": gender}
+        "gender": gender
+    }
 
-  input_df = pd.DataFrame(data,index=[0])
-  input_penguins = pd.concat([input_df, X_raw], axis=0)
+    input_df = pd.DataFrame(data,index=[0])
+    input_penguins = pd.concat([input_df, X_raw], axis=0)
 
     with st.expander("Input data"):
         st.write("**Input data**")
