bbf1aee3fc060d325d8b52ecf067280eb9b86eea
Shalin-777 <chaudharyshalin786@gmail.com>
2025-07-30 09:44:49 -0600

    s

--- app.py
+++ app.py
@@ -72,5 +72,18 @@ with st.sidebar:
 
         st.write("**Combined data**")
         input_penguins
+        # One hot encoding for X
       encode = ["island","sex"]
       df_penguins = pd.get_dummies(input_penguins, prefix = encode)
+      X = df_penguins[1:]
+      input_row = df_penguins[:1]
+      # One hot encoding for y
+      target_mapper = {
+        "Adelie": 0,
+        "Chinstrap": 1,
+        "Gentoo" : 2
+      }
+      def target_encode(val):
+        return target_mapper[val]
+
+      y = y_raw.apply
