e0ed46ab11326dc7d120ca8c4bba5ac1117f43cb
Shalin-777 <chaudharyshalin786@gmail.com>
2025-07-30 09:17:05 -0600

    s

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ inf = df.info()
 print (inf)
 
 with st.expander("Data Visualization"):
-  pass
+  st.scatter_chart(data=df, x = "bill_length_mm", y = "body_mass_g", color = "species")
 
 with st.expander("Input data"):
   pass
